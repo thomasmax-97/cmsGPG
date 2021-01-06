d51987fe013dab19f3d8b01e8cd699154a3d8bf7
Seiwix <manuelplaner@gmail.com>
2021-01-06 20:20:28 +0100

    frontend/teampage

---
+++ pages/team.vue
@@ -0,0 +1,76 @@
+<template>
+
+
+<body>
+
+
+
+<main class="site-content site-content--center page" id="wrapper">
+			<div class="container container--large">
+				<div class="page-heading page-heading--default text-center w-100">
+					
+					<h1 class="page-heading__title h2">Leitung</h1>
+				</div>
+				<div class="row mt-sm-auto mb-sm-auto">
+					<div class="col-sm-6 col-lg-3">
+						<div class="team-item team-item--v4 team-3">
+							<a href="team-overview.html" class="team-item__thumbnail">
+								<div class="team-item__bg-holder">
+									<div class="team-item__bg" style="background-image: url(assets/img/samples/team-selection-character-01-bg.jpg);"></div>
+								</div>
+								<div style="background-image: url(assets/img/samples/team-selection-character-01.png);" class="team-item__img-primary"></div>
+							</a>
+							<span class="team-item__subtitle h6">Necromancers</span>
+							<h2 class="team-item__title">Xenowatch</h2>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3">
+						<div class="team-item team-item--v4 team-1">
+							<a href="team-overview.html" class="team-item__thumbnail">
+								<div class="team-item__bg-holder">
+									<div class="team-item__bg" style="background-image: url(assets/img/samples/team-selection-character-02-bg.jpg);"></div>
+								</div>
+								<div style="background-image: url(assets/img/samples/team-selection-character-02.png);" class="team-item__img-primary"></div>
+							</a>
+							<span class="team-item__subtitle h6">Necromancers</span>
+							<h2 class="team-item__title">League of Heroes</h2>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3">
+						<div class="team-item team-item--v4 team-4">
+							<a href="team-overview.html" class="team-item__thumbnail">
+								<div class="team-item__bg-holder">
+									<div class="team-item__bg" style="background-image: url(../assets/img/samples/team-selection-character-03-bg.jpg);"></div>
+								</div>
+								<div style="background-image: url(../assets/img/samples/team-selection-character-03.png);" class="team-item__img-primary"></div>
+							</a>
+							<span class="team-item__subtitle h6">Necromancers</span>
+							<h2 class="team-item__title">Striker Go</h2>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3">
+						<div class="team-item team-item--v4 ">
+							<a href="team-overview.html" class="team-item__thumbnail">
+								<div class="team-item__bg-holder">
+									<div class="team-item__bg" style="background-image: url(assets/img/samples/team-selection-character-04-bg.jpg);"></div>
+								</div>
+								<div style="background-image: url(assets/img/samples/team-selection-character-04.png);" class="team-item__img-primary"></div>
+							</a>
+							<span class="team-item__subtitle h6">Necromancers</span>
+							<h2 class="team-item__title">Fortdaite</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+<div class="bag" style="background-image: url(assets/img/bg-texture-01.jpg);"></div>		
+</body>
+
+	
+</template>
+
+<style>
+body{
+	background-image: url(../assets/img/bg-texture-01.jpg);
+}
+</style>
