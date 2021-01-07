eac4744b780c9b1576ffdb780613a0dfbd6b4f9c
Seiwix <manuelplaner@gmail.com>
2021-01-07 21:04:10 +0100

    frontend/pages

---
+++ pages/bewerben.vue
@@ -0,0 +1,91 @@
+<template>
+    <body>
+
+<div class="contact-section">
+  <h1>Bewerbung zum Mitglied</h1>
+  <div class="border"></div>
+  <form class="contact-form" action="index.html" method="post">
+    <input type="text" class="contact-form-text" placeholder="Name">
+    <input type="email" class="contact-form-text" placeholder="Email">
+    <textarea class="contact-form-text" placeholder="Your message"></textarea>
+    <input type="submit" class="contact-form-btn" value="Send">
+
+  </form>
+
+
+
+</div>
+
+
+  </body>
+</template>
+<script>
+
+  
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: "montserrat",sans-serif;
+}
+.contact-section{
+  background: url(../assets/img/bg-texture-01.jpg) no-repeat center;
+  background-size: cover;
+  padding: 230px 0;
+}
+.contact-section h1{
+  text-align: center;
+  color: #ddd;
+}
+.border{
+  width: 100px;
+  height: 10px;
+  background: #34495e;
+  margin: 10px auto;
+}
+
+.contact-form{
+  max-width: 600px;
+  margin: auto;
+  padding: 0 10px;
+  overflow: hidden;
+}
+
+.contact-form-text{
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 16px 0;
+  border: 0;
+  background: #111;
+  padding: 20px 40px;
+  outline: none;
+  color: #ddd;
+  transition: 0.5s;
+}
+.contact-form-text:focus{
+  box-shadow: 0 0 10px 4px #34495e;
+}
+textarea.contact-form-text{
+  resize: none;
+  height: 120px;
+}
+.contact-form-btn{
+  float: right;
+  color:white;
+	font-size: 15px;
+	font-weight: 700;
+	border: 1px solid #fdb40e;
+	background: #fdb40e;
+	text-transform: capitalize;
+	padding: 15px 50px 15px;
+}
+.contact-form-btn:hover{
+  text-decoration: none;
+    color: white;
+	border: 1.4px solid white;
+}
+
+</style>
---
+++ pages/ueberuns.vue
@@ -0,0 +1,89 @@
+<template>
+
+<body>
+	<div class="main-bann-section container">
+	<div class="auto-container">
+		<div class="inner-container">
+			<div class="sec-title light col-md-6">
+				<div class="title"></div>
+				<h2>germany Playsgroup <br>
+				<span style="font-size:60px;">Über den verein</span>
+				</h2>
+				<br></br>
+				<div class="text">
+				<p>Die Germanys-Playsgroup e.V.
+			ist ein Zusammenschluss aus Personen,
+			die in ihrer Freizeit, der Leidenschaft des Gaming´s teilt, sowie der Technik.
+			Das Hauptinteresse des Vereins ist es, Personen zu fördern und zu vermitteln.
+			So werden e-Sport Spieler weiter an andere Vereine/Clans vermittelt, um einen Transfermarkt zubilden.
+			Durch die große Vernetzung die bereits seid 2017 besteht, können e-Sports Spieler in ihre Karriere durchstarten.
+			Auch in der Germany-Playsgroup Community ist der e-Sport angekommen, so wurde in der Vergangenheit an mehreren Namenhaften Tunieren der ESL teilgenommen!.					 
+				</p>
+
+				</div>
+				<br>
+				<div class="btn-box"></div>
+				<a href="" class="theme-btn btn-style-one">Mitglied werden</a>
+			</div>
+		</div>
+	</div>
+	</div>
+</body>
+
+</template>
+<style>
+body{
+	background: url(../assets/img/bg-texture-01.jpg)no-repeat center;
+	background-size: cover;
+	font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;
+	
+}
+.main-bann-section{
+	position: relative;
+	padding: 90px 0px 250px;
+	background-repeat: no-repeat;
+	background-position: top center;
+	background-size: cover;
+}
+.main-bann-section .text p{
+	color: white;
+
+}
+p, .text{
+	position: relative;
+	line-height: 1.8em;
+	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	text-align: justify;
+}
+.sec-title.light .title, .sec-title.light h2{
+	color:white;
+
+}
+.btn-style-one{
+	position: relative;
+	line-height: 30px;
+	color:white;
+	font-size: 15px;
+	font-weight: 700;
+	border: 1px solid #fdb40e;
+	background: #fdb40e;
+	text-transform: capitalize;
+	padding: 15px 50px 15px;
+
+}
+.theme-btn{
+	display: inline-block;
+	transition: all 0.3s  ease;
+}
+.btn-style-one:hover{
+	text-decoration: none;
+	color: white;
+	border: 1px solid white;
+}
+
+</style>
+    
+
+
+    
+
