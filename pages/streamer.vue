fc32387cfe2568e51346738bbc2de98628665678
Seiwix <manuelplaner@gmail.com>
2021-01-10 20:19:31 +0100

    frontend/pages: add team1 and contact page

---
+++ pages/streamer.vue
@@ -0,0 +1,50 @@
+<template>
+<body>
+    <main>
+    <div class="team-carousel__item" data-icon="replay">
+							<div class="row">
+								<div class="col-lg-6">
+									<h3 class="player-info-subtitle h5">DaVikinger90</h3>
+									<a href="" class="btn btn-twitch float-right">
+										<i class="fab fa-twitch">&nbsp;</i>Follow Me!
+									</a>
+									<h2 class="player-info-title text-uppercase">Livestream</h2>
+		
+									<!-- Video -->
+									<article class="stream has-post-thumbnail" data-id="playhearthstone" data-controls="true" data-provider="twitch" data-thumbnail="../assets/img/samples/stream-img-01.jpg" data-setsize="true" data-easy-embed>
+										<div class="stream__thumbnail">
+											<img src="../assets/img/samples/stream-img-01.jpg" alt="">
+										</div>
+										<div class="stream__icon"></div>
+										<div class="stream__header">
+											<div class="stream__info">
+												<div class="stream__avatar">
+													<img style="float: left;" src="../assets/img/samples/player-info-page-player-photo-01.png" alt="">
+												</div>
+												<h6 class="stream__title">Davikinger plays &quot;Clash of eternity&quot; <span class="badge badge-danger badge-live">Live</span></h6>
+												<div class="stream__date">August 3rd, 2018</div>
+											</div>
+										</div>
+									</article>
+									<!-- Video / End -->
+		
+	</div>
+                            </div>
+    </div>
+							
+						<!-- Section: Replay / End -->
+</main>		
+                        
+</body>
+
+</template>
+<style>
+body{
+    background-image: url(../assets/img/bg-texture-01.jpg);
+}
+.stream__avatar 
+{
+    right: auto;
+}
+
+</style>
